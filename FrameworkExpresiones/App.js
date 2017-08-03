b3a9fa6e0124588eb5da1af588d4dbf76cfa0c28
SCandelieri <santiagocandelieri@gmail.com>
2017-08-03 11:32:24 -0300

    Create main structure.

---
+++ FrameworkExpresiones/App.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+	$('.content').htmplate('template.html', { nombre: "Santiago", edad: 21 });
+});
---
+++ FrameworkExpresiones/Scripts/jquery-htmplate.js
@@ -0,0 +1,20 @@
+﻿(function ($) {
+	
+	$.fn.htmplate = function(templatePath, data) {
+		var htmlStr = getHtmlTemplate(templatePath);
+		htmlStr = parseHtml(htmlStr, data);
+		this.html(htmlStr);
+		return this;
+	}
+
+	//TODO: Implement function.
+	function getHtmlTemplate(templatePath) {
+		return '<h1>Prueba vacía.</h1>';
+	}
+
+	//TODO: Implement function.
+	function parseHtml(htmlStr, data) {
+		return htmlStr;
+	}
+
+})(jQuery);
