40a73b87a73663fcfcefac3b8f774082e594d7a9
SCandelieri <santiagocandelieri@gmail.com>
2017-08-07 17:20:45 -0300

    Change evaluateExpression
    Return empty string when expression is undefined.

--- FrameworkExpresiones/Scripts/jquery-htmplate.js
+++ FrameworkExpresiones/Scripts/jquery-htmplate.js
@@ -2,7 +2,7 @@
 	
 	$.fn.htmplate = function(templatePath, data, functions) {
 		var self = this;
-		getHtmlTemplate(templatePath, function (response) {
+		getHtmlTemplate(templatePath, function(response) {
 			var htmlStr = parseHtml(response, data, functions);
 			self.html(htmlStr);
 		});
@@ -11,7 +11,7 @@
 
 	//TODO: Change AJAX call to allow cross site. Manage exceptions.
 	function getHtmlTemplate(templatePath, callbackFn) {
-		$.get(templatePath, function (response) {
+		$.get(templatePath, function(response) {
 			callbackFn(response);
 		});
 	}
@@ -28,16 +28,17 @@
 	function getArrayFromExpression(expression) {
 		return expression.slice(2, expression.length - 2)
 						 .split("|")
-						 .map(function (variableName) { return variableName.trim(); });
+						 .map(function(variableName) { return variableName.trim(); });
 	}
 
 	//TODO: Manage exceptions.
 	function evaluateExpression(variableNames, data, functions) {
 		var variableName = variableNames[0];
 		var functionNames = variableNames.slice(1, variableNames.length);
-		return functionNames.reduce(function (acum, functionName) {
+		var expressionResult = functionNames.reduce(function(acum, functionName) {
 			return functions[functionName](acum);
 		}, data[variableName]);
+		return (!expressionResult) ? "" : expressionResult;
 	}
 
 })(jQuery);
